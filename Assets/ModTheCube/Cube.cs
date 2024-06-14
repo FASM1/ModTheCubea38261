a199c4d88228606b039d92f13347282bc4c17945
FASM1 <filipeari@live.com.pt>
2024-06-14 14:34:54 +0100

    Mod the Cube
    2.Download the provided assets and import them into a new Unity Project
    3.Observe the default script functionality
    4.Modify the cube.cs script5.Create a WebGL project showing off your modded cube! "a38261

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    private float horizontalInput;
+    private float verticalInput;
+    public float speed = 10;
+    
+    void Start()
+    {
+        transform.position = new Vector3(0, 4, 1);
+        transform.localScale = Vector3.one * 8f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(0.5f, 1.0f, 1.3f, 0.4f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(80.0f * Time.deltaTime, 30.0f * Time.deltaTime, 50.0f * Time.deltaTime);
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+    }
+}
